bcfa641497499794647048e75ced33a0382e5567
Wanish Sengkhim <wanish45037.2@gmail.com>
2021-04-28 03:51:48 +0700

    Random buttons

--- src/app/words/words.component.ts
+++ src/app/words/words.component.ts
@@ -8,17 +8,57 @@ import word_data from '../_files/word_data.json';
   templateUrl: './words.component.html',
   styleUrls: ['./words.component.css']
 })
+
+
 export class WordsComponent implements OnInit {
 
   wordList: { vocab: String, reading: String, meaning: String }[] = word_data;
 
+
   constructor() {
-   }
+  }
 
 
   ngOnInit(): void {
     this.randomWord();
   }
+
+
+  randomButton(myOBJ, myOBJ_choice1, myOBJ_choice2) {
+
+    var myarray = [myOBJ.meaning, myOBJ_choice1.meaning, myOBJ_choice2.meaning];
+    var random = myarray[Math.floor(Math.random() * myarray.length)];
+    //console.log(random);
+    
+
+    var myarray2 = [myOBJ.meaning, myOBJ_choice1.meaning, myOBJ_choice2.meaning];
+    var random2 = myarray2[Math.floor(Math.random() * myarray2.length)];
+    //console.log(random2);
+    
+
+    var myarray3 = [myOBJ.meaning, myOBJ_choice1.meaning, myOBJ_choice2.meaning];
+    var random3 = myarray3[Math.floor(Math.random() * myarray3.length)];
+    //console.log(random3);
+
+    if (
+      random == null ||
+      random2 == null ||
+      random3 == null ||
+      random == random2 ||
+      random == random3 ||
+      random2 == random3
+      ) {
+      this.randomButton(myOBJ, myOBJ_choice1, myOBJ_choice2);
+    } else {
+      document.getElementById("randomButton").innerHTML = random;
+      document.getElementById("randomButton2").innerHTML = random2;
+      document.getElementById("randomButton3").innerHTML = random3;
+    }
+
+
+  }
+
+
 
   randomWord() {
     //random Word and meaning
@@ -28,7 +68,7 @@ export class WordsComponent implements OnInit {
     var myJSON = JSON.stringify(resultRandom);
     var myOBJ = JSON.parse(myJSON);
     document.getElementById("vocabulary").innerHTML = myOBJ.vocabulary;
-    document.getElementById("meaning").innerHTML = myOBJ.meaning;
+    //document.getElementById("meaning").innerHTML = myOBJ.meaning;
 
     this.randomChoices(myOBJ);
 
@@ -40,26 +80,31 @@ export class WordsComponent implements OnInit {
     var resultRandomChoice1 = this.wordList[randomChoice1];
     var myJSON_choice1 = JSON.stringify(resultRandomChoice1);
     var myOBJ_choice1 = JSON.parse(myJSON_choice1);
-    console.log(myJSON_choice1);
-    document.getElementById("otherChoice1").innerHTML = myOBJ_choice1.meaning;
+    //console.log(myJSON_choice1);
+    //document.getElementById("otherChoice1").innerHTML = myOBJ_choice1.meaning;
+
+    
     //random Choice 2
     var randomChoice2 = Math.floor(Math.random() * this.wordList.length);
     var resultRandomChoice2 = this.wordList[randomChoice2];
     var myJSON_choice2 = JSON.stringify(resultRandomChoice2);
     var myOBJ_choice2 = JSON.parse(myJSON_choice2);
-    console.log(myJSON_choice2);
-    document.getElementById("otherChoice2").innerHTML = myOBJ_choice2.meaning;
+    //console.log(myJSON_choice2);
+    //document.getElementById("otherChoice2").innerHTML = myOBJ_choice2.meaning;
 
     if (
       myOBJ.meaning == null ||
       myOBJ_choice1.meaning == null ||
       myOBJ_choice2.meaning == null ||
-      myOBJ.meaning == myOBJ_choice1.meaning || 
-      myOBJ.meaning == myOBJ_choice2.meaning || 
+      myOBJ.meaning == myOBJ_choice1.meaning ||
+      myOBJ.meaning == myOBJ_choice2.meaning ||
       myOBJ_choice1.meaning == myOBJ_choice2.meaning
-      ) {
+    ) {
       this.randomChoices(myOBJ);
-    } else {}
+    } else { }
+    this.randomButton(myOBJ, myOBJ_choice1, myOBJ_choice2);
   }
 
 }
+
+
